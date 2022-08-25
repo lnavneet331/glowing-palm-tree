f3493a4362a458fce551288adad4c969940a4316
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2022-08-25 15:41:06 +0530

    Vuln fixes

--- acadship/settings.py
+++ acadship/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure =2hlwy6xrn8lh8mfzx#wg_drjtwx78-*&3-d69d&ozq5rlf&h=")
-#SECRET_KEY = 'django-insecure-=2hlwy6xrn8lh8mfzx#wg_drjtwx78-*&3-d69d&ozq5rlf&h='
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
