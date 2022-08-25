d4f151ea353500d7738e13eeee7588bae0e878b6
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2022-08-25 23:27:55 +0530

    update registration

--- data_cleaner.py
+++ data_cleaner.py
@@ -13,7 +13,19 @@ with open("the data of zeus.csv", newline="", encoding="utf8") as file:
             #replace � with " "
             if "�" in row[i]:
                 row[i] = row[i].replace("�", " ")
-
+    counter = 0
+    print(len(raw))
+    new_raw = []
+    #Remove bad data
+    for row in raw:
+        a = row.count("NA")
+        #print(type(a))
+        if a < 3:
+            print(row, a)
+            new_raw.append(row)
+            counter += 1
+    raw = new_raw
+    
     with open("data_clean.csv", "w", newline="", encoding="utf8") as file:
         writer = csv.writer(file)
         writer.writerows(raw)
