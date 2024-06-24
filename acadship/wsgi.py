3d10ff20c6b57b3e74c717188ba4735b1ac79d4b
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2024-06-24 13:04:38 -0700

    Update wsgi.py

--- acadship/wsgi.py
+++ acadship/wsgi.py
@@ -5,10 +5,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acadship.settings')
+
+# application = get_wsgi_application()
+
+
+
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acadship.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
+app = application
