6eb87332af1a3eaa86dccfd9e6514991af3db1e0
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2024-06-25 10:55:40 -0700

    Update settings.py

--- acadship/settings.py
+++ acadship/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['acadship.herokuapp.com', 'localhost', '127.0.0.1', ".vercel.app"]
+ALLOWED_HOSTS = ["*", 'acadship.herokuapp.com', 'localhost', '127.0.0.1', ".vercel.app"]
 
 # Application definition
 
